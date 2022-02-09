e7fc54930585931666071ed3fbdd19b485a195b0
Jonathan Grant <jhgrant@gmail.com>
2022-02-09 13:16:21 -0700

    feat: code skeleton of app views with placeholder buttons/links.

---
+++ views/Settings.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Text, TouchableOpacity, StatusBar, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { styles } from './Styles'
+
+function Settings( { navigation } ) {
+  return (
+      <View style={styles.container}>
+        <Text>Settings</Text>
+        <StatusBar style="auto" />
+        <TouchableOpacity 
+          onPress={() => navigation.navigate('Home')}>
+          <Text>Return to Home</Text>
+        </TouchableOpacity>
+      </View>
+  );
+}
+
+export default Settings
