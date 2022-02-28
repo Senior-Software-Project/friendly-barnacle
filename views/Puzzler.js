f7e0aeb7c3bd8f6279486f008c9e6360b1bee43a
Christian M <seboss123@gmail.com>
2022-02-28 12:08:26 -0700

    OpenTriva API connected

--- views/Puzzler.js
+++ views/Puzzler.js
@@ -1,15 +1,40 @@
 import React from 'react'
-import { Text, TouchableOpacity, StatusBar, View } from 'react-native'
+import { Text, TouchableOpacity, StatusBar, View, TouchableHighlight } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { styles } from './Styles'
 
 // Puzzle Page
-function Puzzler ({ navigation }) {
+function Puzzler({ navigation }) {
+  let [question, setQuestion] = React.useState('')
+  let [incorrect, setIncorrect] = React.useState([])
+  let [type, setType] = React.useState('')
+
+  const fetchApiCall = () => {
+    fetch('https://opentdb.com/api.php?amount=1', {
+      method: 'GET'
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response.results[0])
+        setQuestion(response.results[0].question)
+        setType(response.results[0].type)
+        setIncorrect(response.results[0].incorrect_answers)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
   return (
     <View style={styles.container}>
+      <Text style={styles.text}>{question}</Text>
+      <Text style={styles.text}>{type}</Text>
+      <Text style={styles.text}>{incorrect}</Text>
+      <TouchableHighlight onPress={fetchApiCall}>
+        <Text style={styles.text}>Press to get JSON</Text>
+      </TouchableHighlight>
       <Text style={styles.text}>The Question</Text>
-      <StatusBar style='auto' />
+      <StatusBar style="auto" />
     </View>
   )
 }
