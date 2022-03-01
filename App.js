8b9b889b8856390e0905d8d0ae8d0b19c10f0ff4
Christian M <seboss123@gmail.com>
2022-02-28 17:42:37 -0700

    Changed Splash Screen Background color to match app

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer, DarkTheme } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 // import HomeScreen from './views/Home'
 // import Puzzler from './views/Puzzler'
@@ -10,22 +10,24 @@ import { AnimatedAppLoader } from './views/Splash'
 import Tabs from './Navigation/tabs'
 import { HomeScreen, Puzzler, Settings, Splash, Stats } from './views'
 
+const splashImage = require('././assets/splash.png')
+
 const Stack = createNativeStackNavigator()
 const App = () => {
   return (
-    <NavigationContainer>
-      <AnimatedAppLoader image={require('././assets/splash.png')}>
+    <NavigationContainer theme={DarkTheme}>
+      <AnimatedAppLoader image={splashImage}>
         <Stack.Navigator
           screenOptions={{
             headerShown: false
           }}
-          initialRouteName='Home'
+          initialRouteName="Home"
         >
-          <Stack.Screen name='Home' component={Tabs} />
-          <Stack.Screen name='Puzzler' component={Puzzler} />
-          <Stack.Screen name='Settings' component={Settings} />
-          <Stack.Screen name='Splash' component={Splash} />
-          <Stack.Screen name='Stats' component={Stats} />
+          <Stack.Screen name="Home" component={Tabs} />
+          <Stack.Screen name="Puzzler" component={Puzzler} />
+          <Stack.Screen name="Settings" component={Settings} />
+          <Stack.Screen name="Splash" component={Splash} />
+          <Stack.Screen name="Stats" component={Stats} />
         </Stack.Navigator>
       </AnimatedAppLoader>
     </NavigationContainer>
