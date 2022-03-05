d1f37d0f1b001e99e3d0d4171628fd4b0db857e4
Jonathan Grant <jhgrant@gmail.com>
2022-03-04 23:42:40 -0700

    fix: styling and defects of App.test

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,13 +1,14 @@
-import App, { testFunc } from '../App'
-import { shallow } from 'enzyme';
+import App from '../App'
+import { shallow } from 'enzyme'
+import {describe, expect, test} from '@jest/globals'
 
 describe('<App />', () => {
   test('App should not have lexical errors.', () => {
-    shallow(<App />)
+    expect(shallow(<App />))
   }),
   test('App should not return null.', () => {
     const result = shallow(<App />)
-    expect(typeof(result)).not.toEqual(null)
+    expect(typeof (result)).not.toEqual(null)
   }),
   test('App should return Home as the initial view.', () => {
     const result = JSON.stringify(App())
