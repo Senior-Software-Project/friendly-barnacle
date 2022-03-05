79f841cf81ecc6ec9a6ec2a9b529bfd685f7fd1d
Jonathan Grant <jhgrant@gmail.com>
2022-03-04 22:48:24 -0700

    feat: setup and unit tests of App

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,44 +1,16 @@
-
-/*
-// Reference: https://dev.to/il3ven/fix-warning-in-react-update-was-not-wrapped-in-act-bk6
-// Source: https://github.com/il3ven/react-testing-devto/blob/fixed/src/App.test.js
-import { renderer, screen, waitFor } from 'react-test-renderer' //"@testing-library/react";
-import axios from "axios";
-import App from "../App";
-
-jest.mock("axios");
-
-test("renders learn react link", async () => {
-  axios.get.mockImplementation(() => {
-    return {
-      data: {
-        userId: 1,
-        id: 1,
-        title: "delectus aut autem",
-        completed: false,
-      },
-    };
-  });
-
-  renderer.create(<App />);
-
-  expect(screen.getByText(/React App/i)).toBeInTheDocument();
-  await waitFor(() => {
-    expect(screen.getByTestId("title").innerHTML).toBe(
-      "Title: delectus aut autem"
-    );
-  });
-});
-*/
-
-// import React from 'react'
-import renderer from 'react-test-renderer'
-import App from '../App'
+import App, { testFunc } from '../App'
+import { shallow } from 'enzyme';
 
 describe('<App />', () => {
-  test('has many children', async () => {
-    const tree = renderer.create('<App />').toJSON()
-    // expect(tree).toBeNull()
-    // expect(tree.children.length).toBe(5)
+  test('App should not have lexical errors.', () => {
+    shallow(<App />)
+  }),
+  test('App should not return null.', () => {
+    const result = shallow(<App />)
+    expect(typeof(result)).not.toEqual(null)
+  }),
+  test('App should return Home as the initial view.', () => {
+    const result = JSON.stringify(App())
+    expect(result).toMatch(/\"initialRouteName\":\"Home\"/)
   })
 })
