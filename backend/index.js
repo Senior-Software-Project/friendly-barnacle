018fddb500896e5a48eb4eece7a1e08077324650
gordong-msud <gordong@msudenver.edu>
2022-03-16 09:14:32 -0600

    corrected linter errors

--- backend/index.js
+++ backend/index.js
@@ -1,46 +1,32 @@
 /* Code is based on material presented in Pluralsight course, "Play by Play: Building a Node Web API with Sam Artioli and John Papa" at:
         https://app.pluralsight.com/library/courses/play-by-play-node-web-api-john-papa-sam-artioli/table-of-contents (subscription req'd)
-*/      
+*/
 
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from 'express'
+import bodyParser from 'body-parser'
+
+import userStats from './user-stats.js'
 
 const app = express()
 
 const hostname = '127.0.0.1'
 const port = 3000
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: true
-}));
+}))
 
-import userStats from './user-stats.js';
-
-var stats = userStats(app);
-
-/*
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-  //res.json('hello: world');
-});
-*/
+const stats = userStats(app)
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
+/*   userStatsRoutesLoaded = false
+  if (typeof stats != "undefined") {
+    userStatsRoutesLoaded = true
+  } */
+  if (typeof stats === 'undefined') {
+    console.log(`Server running at http://${hostname}:${port}/, userStats routes loaded`)
+  } else {
+    console.log(`Server running at http://${hostname}:${port}/`)
+  }
 })
-
-/*
-import { createServer } from 'http';
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-}).listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-*/
--- backend/user-stats.js
+++ backend/user-stats.js
@@ -1,48 +1,49 @@
-import _ from 'lodash';
+import _ from 'lodash'
 
-export default function(app) {
+export default function (app) {
+  const _userStats = []
 
-    var _userStats = [];
+  /* Create */
+  app.post('/userStats', (req, res) => {
+    _userStats.push(req.body)
+    res.json({ info: 'userStat added successfully' })
+  })
 
-    /* Create */
-    app.post('/userStats', (req, res) => {
-        _userStats.push(req.body);
-        res.json({info: 'userStat added successfully'});
-    });
+  /* Read */
+  app.get('/userStats', (req, res) => {
+    res.send(_userStats)
+  })
 
-    /* Read */
-    app.get('/userStats', (req, res) => {
-        res.send(_userStats)
-    })
+  app.get('/userStats/:id', (req, res) => {
+    const index = _.findIndex(
+      _userStats,
+      (o) => {
+        return o.userId === req.params.id
+      }
+    )
 
-    app.get('/userStats/:id', (req, res) => {
-        var index = _.findIndex(
-            _userStats,
-            (o) => {
-                return o.userId == req.params.id;
-            }
-        );
+    // res.json(`index ${index} found for param ${req.params.id}`);
+    res.json(_userStats[index])
+  })
 
-        //res.json(`index ${index} found for param ${req.params.id}`);
-        res.json(_userStats[index]);
-    });
+  /* Update */
+  app.put('/userStats/:id', (req, res) => {
+    const index = _.findIndex(
+      _userStats,
+      (o) => {
+        return o.userId === req.params.id
+      }
+    )
+    _.merge(_userStats[index], req.body)
+  })
 
-    /* Update */
-    app.put('/userStats/:id', (req, res) => {
-        var index = _.find(
-            _userStats,
-            () => {
-                userId: req.params.id
-            }
-        );
-        _.merge(_userStats[index], req.body);
-    });
-
-    /* Delete */
-    app.delete('/userStats/:id', (req, res) => {
-        _.remove(_userStats, (userStat) => {
-            userStat.Id === req.params.id;
-        });
-        res.json({info: 'userStat removed successfully'});
-    });
+  /* Delete */
+  app.delete('/userStats/:id', (req, res) => {
+    _.remove(
+      _userStats,
+      (userStat) => {
+        return userStat.Id === req.params.id
+      })
+    res.json({ info: 'userStat removed successfully' })
+  })
 }
