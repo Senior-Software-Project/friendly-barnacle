d9a44cc23be7e17eacc601f2560d5857943ae5ab
JonathanGrant92 <jhgrant@gmail.com>
2022-03-25 11:47:51 -0600

    feat: more tests of Home view

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,9 +1,40 @@
 import App from '../App'
 import { describe, expect, test } from '@jest/globals'
+import getAppStack from '../App'
+import {NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
 
 describe('<App />', () => {
   test('App should return Home as the initial view.', () => {
     const result = JSON.stringify(App())
     expect(result).toMatch('"initialRouteName":"Home"')
+  })
+  test('App stack returns NavigationStack', () => {
+    expect(App).toStrictEqual(getAppStack)
+    expect(JSON.stringify(getAppStack())).toMatch('"initialRouteName":"Home"')
+    expect(typeof(getAppStack)).toBe(typeof(() => {}))
+    expect(getAppStack).toBeTruthy()
+  })
+  test('Mock the app', () => {
+    const Stack = createNativeStackNavigator()
+    const component = {}
+    const params = {}
+    const MockedNavigator = () => {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="MockedScreen"
+              component={component}
+              initialParams={params}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      )
+    }
+    expect(Stack).toBe(Stack)
+  })
+  test('Compare everything', () => {
+    expect(JSON.stringify(getAppStack())).toBe(JSON.stringify(App()))
   })
 })
