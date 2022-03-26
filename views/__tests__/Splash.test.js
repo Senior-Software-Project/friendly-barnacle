d7365fc0609c8897b75e739792fcecf5c3682ba1
Jonathan Grant <jhgrant@gmail.com>
2022-03-26 12:31:52 -0600

    feat: Splash test setup

--- views/__tests__/Splash.test.js
+++ views/__tests__/Splash.test.js
@@ -1,3 +1,4 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Splash from '../Splash'
 import { describe, expect, test } from '@jest/globals'
 import { create } from 'react-test-renderer'
@@ -8,8 +9,11 @@ describe('<Splash />', () => {
   })
   test('Splash should not return null.', () => {
     expect(typeof (create(Splash))).not.toEqual(null)
+    const tree = JSON.stringify(create(Splash))
+    expect(tree).toMatch('null')
   })
-  test('Splash should shuffle array.', () => {
+  test('Splash should return a View component.', () => {
+    const nav = createNativeStackNavigator().Navigator
     const tree = JSON.stringify(create(Splash))
     expect(tree).toMatch('null')
   })
