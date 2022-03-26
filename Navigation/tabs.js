8a4c23578c57dbc8798a3641cf25ac2521271538
Jonathan Grant <jhgrant@gmail.com>
2022-03-26 15:00:40 -0600

    feat: refactor tabs jsx tabBarIcon

--- Navigation/tabs.js
+++ Navigation/tabs.js
@@ -6,88 +6,54 @@ import { images } from '../components'
 
 const Tab = createBottomTabNavigator()
 
+function MenuIcon(icon) {
+  return (
+     ({ focused }) => (
+      <Image
+        source = {icon}
+        resizeMode = "contain"
+        style = {{
+          width: 45,
+          height: 45,
+          tintColor: focused ? '#5A57F7' : '#FFFFFF'
+        }}
+      />
+    )
+  )
+}
+
 const Tabs = () => {
   return (
-        <Tab.Navigator
-            screenOptions={{
-              tabBarShowLabel: false,
-              tabBarStyle: {
-                backgroundColor: '#34344A',
-                borderTopWidth: 0
-              },
-              headerShown: false
-
-            }}
-        >
-            <Tab.Screen
-                name = "Alarm"
-                component = {HomeScreen}
-                options = {{
-                  tabBarIcon: ({ focused }) => (
-                        <Image
-                            source = {images.alarm}
-                            resizeMode = "contain"
-                            style = {{
-                              width: 45,
-                              height: 45,
-                              tintColor: focused ? '#5A57F7' : '#FFFFFF'
-
-                            }}
-                        />
-                  )
-                }}
-      />
-      <Tab.Screen
-        name='Puzzler'
-        component={Puzzler}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={images.puzzle}
-              resizeMode='contain'
-              style={{
-                width: 45,
-                height: 45,
-                tintColor: focused ? '#5A57F7' : '#FFFFFF'
-              }}
-            />
-          )
-        }}
+    <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: '#34344A',
+          borderTopWidth: 0
+        },
+        headerShown: false
+      }}
+    >
+        <Tab.Screen
+          name = 'Alarm'
+          component = {HomeScreen}
+          options = {{tabBarIcon: MenuIcon(images.alarm)}}
         />
-            <Tab.Screen
-                name = "Settings"
-                component = {Settings}
-                options = {{
-                  tabBarIcon: ({ focused }) => (
-                        <Image
-                            source = {images.settings}
-                            resizeMode = "contain"
-                            style = {{
-                              width: 50,
-                              height: 50,
-                              tintColor: focused ? '#5A57F7' : '#FFFFFF'
-                            }}
-                        />
-                  )
-                }}
-            />
-      <Tab.Screen
-        name='Stats'
-        component={Stats}
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Image
-              source={images.stats}
-              resizeMode='contain'
-              style={{
-                width: 50,
-                height: 50,
-                tintColor: focused ? '#5A57F7' : '#FFFFFF'
-              }}
-            />
-          )
-        }}
-      />
+        <Tab.Screen
+          name = 'Puzzler'
+          component = {Puzzler}
+          options = {{tabBarIcon: MenuIcon(images.puzzle)}}
+        />
+        <Tab.Screen
+          name = 'Settings'
+          component = {Settings}
+          options = {{tabBarIcon: MenuIcon(images.settings)}}
+        />
+        <Tab.Screen
+          name = 'Stats'
+          component = {Stats}
+          options = {{tabBarIcon: MenuIcon(images.stats)}}
+        />
     </Tab.Navigator>
   )
 }
