6475656c9a7a1da6289deeaf3afdf7aef2fe5a35
JonathanGrant92 <jhgrant@gmail.com>
2022-04-13 18:33:36 -0600

    fix: style

--- views/Settings.js
+++ views/Settings.js
@@ -1,48 +1,48 @@
 import React, { useState } from 'react'
-import { Text, StatusBar, View, Picker } from 'react-native'
+import { Text, View, Picker } from 'react-native'
 import { styles } from './Styles'
 import { decode } from 'html-entities'
 
 const categories = {
   '': 'Any Category',
-  '9': 'General Knowledge',
-  '10': 'Books',
-  '11': 'Film',
-  '12': 'Music',
-  '13': 'Musicals &amp; Theatres',
-  '14': 'Television',
-  '15': 'Video Games',
-  '16': 'Board Games',
-  '17': 'Science &amp; Nature',
-  '18': 'Computers',
-  '19': 'Mathematics',
-  '20': 'Mythology',
-  '21': 'Sports',
-  '22': 'Geography',
-  '23': 'History',
-  '24': 'Politics',
-  '25': 'Art',
-  '26': 'Celebrities',
-  '27': 'Animals',
-  '28': 'Vehicles',
-  '29': 'Comics',
-  '30': 'Gadgets',
-  '31': 'Japanese Anime &amp; Manga',
-  '32': 'Cartoon &amp; Animations'
+  9: 'General Knowledge',
+  10: 'Books',
+  11: 'Film',
+  12: 'Music',
+  13: 'Musicals &amp; Theatres',
+  14: 'Television',
+  15: 'Video Games',
+  16: 'Board Games',
+  17: 'Science &amp; Nature',
+  18: 'Computers',
+  19: 'Mathematics',
+  20: 'Mythology',
+  21: 'Sports',
+  22: 'Geography',
+  23: 'History',
+  24: 'Politics',
+  25: 'Art',
+  26: 'Celebrities',
+  27: 'Animals',
+  28: 'Vehicles',
+  29: 'Comics',
+  30: 'Gadgets',
+  31: 'Japanese Anime &amp; Manga',
+  32: 'Cartoon &amp; Animations'
 }
-var categoryKey = ''
+let categoryKey = ''
 
-function getCategory(){
+function getCategory () {
   return categoryKey
 }
 
-function setCategory(key){
+function setCategory (key) {
   categoryKey = key
 }
 
-let category_options = []
-for (let cat in categories) {
-  category_options.push(<Picker.Item label={decode(categories[cat])} value={cat} key={cat} />)
+const categoryOptions = []
+for (const cat in categories) {
+  categoryOptions.push(<Picker.Item label={decode(categories[cat])} value={cat} key={cat} />)
 }
 
 function Settings () {
@@ -50,8 +50,8 @@ function Settings () {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Category:</Text>
-      <Picker style={{height: 20, width: '80%', weight: 10, color: 'white', textAlign: 'center'}} selectedValue={catKey} onValueChange={(cat) => {setCategoryKey(cat);setCategory(cat)}}>
-        {category_options}
+      <Picker style={{ height: 20, width: '80%', weight: 10, color: 'white', textAlign: 'center' }} selectedValue={catKey} onValueChange={(cat) => { setCategoryKey(cat); setCategory(cat) }}>
+        {categoryOptions}
       </Picker>
     </View>
   )
